2606398316eeaaa1147c6e4bb5e1223464677080
monayemhossain90 <monayemhossain1990.com>
2023-10-17 13:44:23 +0600

    Ready for new project

--- src/models/Users/UsersModel.js
+++ src/models/Users/UsersModel.js
@@ -1,13 +1,13 @@
 const  mongoose=require('mongoose');
-const DataSchema=mongoose.Schema({
+const UserSchema=mongoose.Schema({
     email:{type:String,unique:true},
     firstName:{type:String},
     lastName:{type:String},
-    mobile:{type:String},
+    phone:{type:String},
     password:{type:String},
     photo:{type:String},
     createdDate:{type:Date,default:Date.now()}
 },{versionKey:false});
-const UsersModel=mongoose.model('users',DataSchema);
+const UsersModel=mongoose.model('users',UserSchema);
 module.exports=UsersModel
 
